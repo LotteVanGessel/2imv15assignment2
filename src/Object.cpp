dcc2090224d6821c49940a5ba2301e302f5bd07e
Lotte Gessel, van <l.s.v.gessel@student.tue.nl>
2022-06-19 08:58:08 +0200

    Fix small mistake. Still does not work

--- src/Object.cpp
+++ src/Object.cpp
@@ -37,7 +37,7 @@ void Object::draw(float h) {
 	glEnd();
 }
 
-void Object::setCenter(int cX, int cY)
+void Object::setCenter(int cX, int cY, int N)
 {
 	if (std::abs(cX - cenX) > 1)
 	{
@@ -58,6 +58,22 @@ void Object::setCenter(int cX, int cY)
 	else {
 		vely = cenY - cY;
 		cenY = cY;
+	}
+	if (cenX - size < 0)
+	{
+		cenX = size;
+	}
+	if (cenY - size < 0)
+	{
+		cenY = size;
+	}
+	if (cenX + size > N)
+	{
+		cenX = N - size;
+	}
+	if (cenY + size > N)
+	{
+		cenY = N - size;
 	}
 }
 
@@ -94,63 +110,110 @@ void Object::setBound(int N, int b, float* mat)
 	the square does not yet apply force on the fluid while it moves. 
 	It generally does not seem to work
 */
+
 void Object::force(float* u, float* v, float* dens, int N)
 {
-	
+	// X velocity is negative. 
+	// So we are looking at the xo side  of the square
 	if (velx < 0) 
 	{
 		int i = cenX - size;
 		for (int j = cenY - size + 1; j < cenY + size; j++)
 		{
 			dens[IX(i - 1, j)] += dens[IX(i, j)];
-			u[IX(i - 1, j)] -=  dens[IX(i, j)];
 			dens[IX(i, j)] = 0;
+			u[IX(i - 1, j)] -= dens[IX(i, j)];
 		}
+		
+		// Y velocity is positive 
+		// So we are looking at the y1 side of the square. So vx neg, vy pos means upper left corner. 
+
 		if (vely > 0)
 		{
 			dens[IX(cenX - size, cenY + size + 1)] += 0.5f * dens[IX(cenX - size, cenY + size)];
 			dens[IX(cenX - size - 1, cenY + size)] += 0.5f * dens[IX(cenX - size, cenY + size)];
-			v[IX(cenX - size, cenY + size + 1)] += 0.5f * dens[IX(cenX - size, cenY + size)];
-			u[IX(cenX - size - 1, cenY + size)] -= 0.5f * dens[IX(cenX - size, cenY + size)];
 			dens[IX(cenX - size - 1, cenY - size)] += dens[IX(cenX - size, cenY - size)];
+			v[IX(cenX - size - 1, cenY + size)] += 0.5f * dens[IX(cenX - size, cenY + size)];
+			v[IX(cenX - size, cenY + size + 1)] += 0.5f * dens[IX(cenX - size, cenY + size)];
+			v[IX(cenX - size - 1, cenY - size)] += dens[IX(cenX - size, cenY - size)];
 		}
+		// Y velocity is negative 
+		// So we are looking at the y0 side of the sqyare. So vx neg, vy neg means bottom left corner
 		if (vely < 0)
 		{
 			dens[IX(cenX - size - 1, cenY - size)] += 0.5f * dens[IX(cenX - size, cenY - size)];
 			dens[IX(cenX - size, cenY - size - 1)] += 0.5f * dens[IX(cenX - size, cenY - size)];
 			dens[IX(cenX - size - 1, cenY + size)] +=  dens[IX(cenX - size, cenY + size)];
-			v[IX(cenX - size, cenY + size - 1)] -= 0.5f * dens[IX(cenX - size, cenY - size)];
-			u[IX(cenX - size - 1, cenY + size)] -= 0.5f * dens[IX(cenX - size, cenY - size)];
+			v[IX(cenX - size - 1, cenY - size)] -= 0.5f * dens[IX(cenX - size, cenY - size)];
+			v[IX(cenX - size, cenY - size - 1)] += 0.5f * dens[IX(cenX - size, cenY - size)];
+			v[IX(cenX - size - 1, cenY + size)] += dens[IX(cenX - size, cenY + size)];
+		}
+		if (vely == 0)
+		{
+			dens[IX(cenX - size - 1, cenY - size)] += dens[IX(cenX - size, cenY - size)];
+			dens[IX(cenX - size - 1, cenY + size)] += dens[IX(cenX - size, cenY + size)];
 		}
 	}
 
+	//Vx is positive
 	if (velx > 0)
 	{
 		int i = cenX + size;
 		for (int j = cenY - size + 1; j < cenY + size; j++)
 		{
 			dens[IX(i + 1, j)] += dens[IX(i, j)];
-			u[IX(i + 1, j)] += 2 * dens[IX(i, j)];
 			dens[IX(i, j)] = 0;
+			u[IX(i + 1, j)] += dens[IX(i, j)];
 		}
+		// Y velocity is positive
+		// So we are looking at he yo side of teh square. So vx pos, vy pos means upper right corner. 
 		if (vely > 0)
 		{
 			dens[IX(cenX + size + 1, cenY + size)] += 0.5f * dens[IX(cenX + size, cenY + size)];
 			dens[IX(cenX + size, cenY + size + 1 )] += 0.5f * dens[IX(cenX + size, cenY + size)];
 			dens[IX(cenX + size + 1 , cenY - size)] += dens[IX(cenX + size, cenY - size)];
-
-
+			v[IX(cenX + size + 1, cenY + size)] += 0.5f * dens[IX(cenX + size, cenY + size)];
+			v[IX(cenX + size, cenY + size + 1)] += 0.5f * dens[IX(cenX + size, cenY + size)];
+			v[IX(cenX + size + 1, cenY - size)] += dens[IX(cenX + size, cenY - size)];
+			dens[IX(cenX + size, cenY - size)] = 0;
+			dens[IX(cenX + size, cenY + size)] = 0;
 		}
+		//Y velocity is negative
+		// So we are looking at the y1 side of the square. So vx pos, vy neg means bottom right corner. 
 		if (vely < 0)
 		{
-			dens[IX(cenX + size + 1, cenY + size)] += 0.5f * dens[IX(cenX + size, cenY + size)];
+			dens[IX(cenX + size + 1, cenY - size)] += 0.5f * dens[IX(cenX + size, cenY - size)];
 			dens[IX(cenX + size, cenY - size - 1)] += 0.5f * dens[IX(cenX + size, cenY - size)];
+			dens[IX(cenX + size + 1, cenY + size)] += dens[IX(cenX + size, cenY + size)];
+			v[IX(cenX + size + 1, cenY - size)] += 0.5f * dens[IX(cenX + size, cenY - size)];
+			v[IX(cenX + size, cenY - size - 1)] += 0.5f * dens[IX(cenX + size, cenY - size)];
+			v[IX(cenX + size + 1, cenY + size)] += dens[IX(cenX + size, cenY + size)];
+			dens[IX(cenX + size, cenY - size)] = 0;
+			dens[IX(cenX + size, cenY + size)] = 0;
+		}
+		if (vely == 0)
+		{
 			dens[IX(cenX + size + 1, cenY - size)] += dens[IX(cenX + size, cenY - size)];
+			dens[IX(cenX + size + 1, cenY + size)] += dens[IX(cenX + size, cenY + size)];
+			dens[IX(cenX + size, cenY - size)] = 0;
+			dens[IX(cenX + size, cenY + size)] = 0;
 		}
 	}
 
 	if (vely < 0)
 	{
+		int i = cenY - size;
+		for (int j = cenX - size + 1; j < cenX + size; j++)
+		{
+			dens[IX(j, i - 1)] += dens[IX(j, i)];
+			v[IX(j, i - 1)] += dens[IX(j, i)];
+			dens[IX(j, i)] = 0;
+		}
+		if (velx == 0)
+		{
+			dens[cenX - size, cenY - size - 1] += dens[cenX - size, cenY - size];
+			dens[cenX - size, cenY - size] = 0;
+		}
 		
 	}
 
@@ -159,11 +222,31 @@ void Object::force(float* u, float* v, float* dens, int N)
 		int i = cenY + size;
 		for (int j = cenX - size + 1; j < cenX + size; j++)
 		{
-			dens[IX(j + 1, i)] += dens[IX(j, i)];
+			dens[IX(j, i + 1)] += dens[IX(j, i)];
+			v[IX(j, i + 1)] += dens[IX(j, i)];
 			dens[IX(j, i)] = 0;
+		}
+		if (velx == 0)
+		{
+			dens[cenX - size, cenY + size + 1] += dens[cenX - size, cenY + size];
+			dens[cenX - size, cenY + size] = 0;
 		}
 	}
 	
+	/*if (vely == 0 && velx == 0)
+	{
+		for (int i = cenX - size; i < cenX + size + 1; i++)
+		{
+			dens[IX(i, cenY - size - 1)] += dens[IX(i, cenY - size)];
+			dens[IX(i, cenY + size + 1)] += dens[IX(i, cenY + size)];
+		}
+
+		for (int i = cenY - size; i < cenY + size + 1; i++)
+		{
+			dens[IX(cenX - size - 1, i)] += dens[IX(cenX - size, i)];
+			dens[IX(cenX + size + 1, i)] += dens[IX(cenX + size, i)];
+		}
+	}*/
 }
 
 void Object::velocity(int vx, int vy)
